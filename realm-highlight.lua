a6bd7e679eabcce3f861d60d4c493e456ccc3d28
Matus Tuhy <matustuhy@gmail.com>
2021-04-01 15:17:57 +0200

    show frame if drakthul player appear

--- realm-highlight.lua
+++ realm-highlight.lua
@@ -1,15 +1,36 @@
-local frame = CreateFrame("FRAME", "RealmHighlightFrame");
-frame:RegisterEvent("NAME_PLATE_UNIT_ADDED");
+local f = CreateFrame("Frame",nil,UIParent)
+f:SetFrameStrata("BACKGROUND")
+f:SetWidth(128) -- Set these to whatever height/width is needed 
+f:SetHeight(64) -- for your Texture
+
+local t = f:CreateTexture(nil,"BACKGROUND")
+t:SetTexture("Interface\\Glues\\CharacterCreate\\UI-CharacterCreate-Factions.blp")
+t:SetAllPoints(f)
+f.texture = t
+
+f:SetPoint("CENTER",0,0)
+f:Hide()
+
+f:RegisterEvent("NAME_PLATE_UNIT_ADDED");
+
+local enemies = {}
+
 local function eventHandler(self, event, ...)
-    local nameplateid = ...
-    local nameplateUnitGuid = UnitGUID(nameplateid)
-    local unitName = GetUnitName(nameplateid, true)
-    local name, realm = strsplit("-", unitName)
+    if (event == "NAME_PLATE_UNIT_ADDED") then
+        local nameplateid = ...
+        local nameplateUnitGuid = UnitGUID(nameplateid)
+        local unitName = GetUnitName(nameplateid, true)
+        local name, realm = strsplit("-", unitName)
 
-    -- print(name, realm);
+        -- print(name, realm);
 
-    if (realm == "Drak'thul") then
-        print(unitName)
+        if (realm == "Drak'thul") then
+            f:Show()
+        end
+
+        if (realm == "Turalyon") then
+            f:Show()
+        end
     end
 end
-frame:SetScript("OnEvent", eventHandler);
+f:SetScript("OnEvent", eventHandler);
